c7906c575679361ec10884b0df097b57e3428829
B R T Xen <b23prodtm@users.sourceforge.net>
2026-01-22 02:26:37 +0100

    help tag image

--- docker-tag.sh
+++ docker-tag.sh
@@ -1,9 +1,9 @@
-if [ "$#" -lt 2 ]; then echo "Usage: $0 tag version"; exit 0; fi
-tag=$1
+if [ "$#" -lt 2 ]; then echo "Usage: $0 image version_tag"; exit 0; fi
+image=$1
 VERSION=$2
-docker tag "$tag" docker.io/bprtkop/sysbox-deploy-k8s:latest
-docker tag "$tag" "docker.io/bprtkop/sysbox-deploy-k8s:$VERSION"
-docker tag "$tag" "docker.io/bprtkop/sysbox-deploy-k8s:${VERSION}_amd64"
+docker tag "$image" docker.io/bprtkop/sysbox-deploy-k8s:latest
+docker tag "$image" "docker.io/bprtkop/sysbox-deploy-k8s:$VERSION"
+docker tag "$image" "docker.io/bprtkop/sysbox-deploy-k8s:${VERSION}_amd64"
 docker push -a docker.io/bprtkop/sysbox-deploy-k8s
 docker manifest create bprtkop/sysbox-deploy-k8s:latest --amend "bprtkop/sysbox-deploy-k8s:${VERSION}_amd64"
 docker manifest push bprtkop/sysbox-deploy-k8s:latest
