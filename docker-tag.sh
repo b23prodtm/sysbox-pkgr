ca1a9a45f80772576bb9649e4633dfec4d02a0f8
B R T Xen <b23prodtm@users.sourceforge.net>
2026-01-22 02:30:24 +0100

    help tag arch

--- docker-tag.sh
+++ docker-tag.sh
@@ -5,7 +5,7 @@ docker tag "$image" docker.io/bprtkop/sysbox-deploy-k8s:latest
 docker tag "$image" "docker.io/bprtkop/sysbox-deploy-k8s:$VERSION"
 docker tag "$image" "docker.io/bprtkop/sysbox-deploy-k8s:${VERSION}_$(arch)"
 docker push -a docker.io/bprtkop/sysbox-deploy-k8s
-docker manifest create bprtkop/sysbox-deploy-k8s:latest --amend "bprtkop/sysbox-deploy-k8s:${VERSION}_amd64"
+docker manifest create bprtkop/sysbox-deploy-k8s:latest --amend "bprtkop/sysbox-deploy-k8s:${VERSION}_$(arch)"
 docker manifest push bprtkop/sysbox-deploy-k8s:latest
-docker manifest create bprtkop/sysbox-deploy-k8s:"$VERSION" --amend "bprtkop/sysbox-deploy-k8s:${VERSION}_amd64"
+docker manifest create bprtkop/sysbox-deploy-k8s:"$VERSION" --amend "bprtkop/sysbox-deploy-k8s:${VERSION}_$(arch)"
 docker manifest push bprtkop/sysbox-deploy-k8s:"$VERSION"
