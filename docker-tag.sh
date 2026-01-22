0f1358c9f2333760cc5e30e0ee3982f80f159c7d
B R T Xen <b23prodtm@users.sourceforge.net>
2026-01-22 03:02:20 +0100

    version arch manifest

--- docker-tag.sh
+++ docker-tag.sh
@@ -1,11 +1,14 @@
 if [ "$#" -lt 2 ]; then echo "Usage: $0 image version_tag"; exit 0; fi
 image=$1
 VERSION=$2
+ARCH=amd64
 docker tag "$image" docker.io/bprtkop/sysbox-deploy-k8s:latest
 docker tag "$image" "docker.io/bprtkop/sysbox-deploy-k8s:$VERSION"
-docker tag "$image" "docker.io/bprtkop/sysbox-deploy-k8s:${VERSION}_$(arch)"
+docker tag "$image" "docker.io/bprtkop/sysbox-deploy-k8s:${VERSION}_$ARCH"
 docker push -a docker.io/bprtkop/sysbox-deploy-k8s
-docker manifest create bprtkop/sysbox-deploy-k8s:latest --amend "bprtkop/sysbox-deploy-k8s:${VERSION}_$(arch)"
-docker manifest push bprtkop/sysbox-deploy-k8s:latest
-docker manifest create bprtkop/sysbox-deploy-k8s:"$VERSION" --amend "bprtkop/sysbox-deploy-k8s:${VERSION}_$(arch)"
-docker manifest push bprtkop/sysbox-deploy-k8s:"$VERSION"
+docker manifest create docker.io/bprtkop/sysbox-deploy-k8s:latest \
+--amend "docker.io/bprtkop/sysbox-deploy-k8s:${VERSION}_$ARCH"
+docker manifest push docker.io/bprtkop/sysbox-deploy-k8s:latest
+docker manifest create docker.io/bprtkop/sysbox-deploy-k8s:"$VERSION" \
+--amend "docker.io/bprtkop/sysbox-deploy-k8s:${VERSION}_$ARCH"
+docker manifest push docker.io/bprtkop/sysbox-deploy-k8s:"$VERSION"
