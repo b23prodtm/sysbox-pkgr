ab36a3e41dfe250340b0ceb707427a1c8d26a140
TiB Rkt Arimana <5566338+b23prodtm@users.noreply.github.com>
2026-01-26 23:52:22 +0100

    Update docker-tag.sh

--- docker-tag.sh
+++ docker-tag.sh
@@ -1,14 +1,15 @@
 if [ "$#" -lt 2 ]; then echo "Usage: $0 image version_tag"; exit 0; fi
 image=$1
 VERSION=$2
-ARCH=amd64
-docker tag "$image" docker.io/bprtkop/sysbox-deploy-k8s:latest
-docker tag "$image" "docker.io/bprtkop/sysbox-deploy-k8s:$VERSION"
-docker tag "$image" "docker.io/bprtkop/sysbox-deploy-k8s:${VERSION}_$ARCH"
-docker push -a docker.io/bprtkop/sysbox-deploy-k8s
-docker manifest create docker.io/bprtkop/sysbox-deploy-k8s:latest \
---amend "docker.io/bprtkop/sysbox-deploy-k8s:${VERSION}_$ARCH"
-docker manifest push docker.io/bprtkop/sysbox-deploy-k8s:latest
-docker manifest create docker.io/bprtkop/sysbox-deploy-k8s:"$VERSION" \
---amend "docker.io/bprtkop/sysbox-deploy-k8s:${VERSION}_$ARCH"
-docker manifest push docker.io/bprtkop/sysbox-deploy-k8s:"$VERSION"
+ARCH="$(dpkg --print-architecture)"
+DOCKER_USER=${DOCKER_USER:-"bprtkop"}
+docker tag "$image" docker.io/$DOCKER_USER/sysbox-deploy-k8s:latest
+docker tag "$image" "docker.io/$DOCKER_USER/sysbox-deploy-k8s:$VERSION"
+docker tag "$image" "docker.io/$DOCKER_USER/sysbox-deploy-k8s:${VERSION}_$ARCH"
+docker push -a docker.io/$DOCKER_USER/sysbox-deploy-k8s
+docker manifest create docker.io/$DOCKER_USER/sysbox-deploy-k8s:latest \
+--amend "docker.io/$DOCKER_USER/sysbox-deploy-k8s:${VERSION}_$ARCH"
+docker manifest push docker.io/$DOCKER_USER/sysbox-deploy-k8s:latest
+docker manifest create docker.io/$DOCKER_USER/sysbox-deploy-k8s:"$VERSION" \
+--amend "docker.io/$DOCKER_USER/sysbox-deploy-k8s:${VERSION}_$ARCH"
+docker manifest push docker.io/$DOCKER_USER/sysbox-deploy-k8s:"$VERSION"
